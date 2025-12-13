dd5b1320f35780a8c73cdd75dc03c40095074e79
huanchen-stack <huanchen@usc.edu>
2025-12-13 10:29:43 +0000

    add original

--- llada/generate.py
+++ llada/generate.py
@@ -24,9 +24,9 @@ import numpy as np
 import torch.nn.functional as F
 import os
 from transformers import AutoTokenizer, AutoModel
-# from model.modeling_llada import LLaDAModelLM
+from model.modeling_llada import LLaDAModelLM
 # from model.modeling_llada_sgl_kernel import LLaDAModelLM
-from model.modeling_llada_cudagraph import LLaDAModelLM 
+# from model.modeling_llada_cudagraph import LLaDAModelLM 
 
 from torch.cuda import nvtx
 
@@ -447,7 +447,7 @@ def main():
     with torch.inference_mode():
         nvtx.range_push("INFER")
 
-        REPEAT = 1
+        REPEAT = 10
         print("warming up...")
         [ generate_with_prefix_cache(model, input_ids, steps=128, gen_length=128, block_length=32, temperature=0., remasking='low_confidence') for _ in range(REPEAT) ]
         import time
